6d7801f644c8ad4dc2a7e1db61308aa6c7cf8e24
Rizqy12 <92738323+Rizqy12@users.noreply.github.com>
2021-11-09 16:34:51 +0700

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,102 +1,120 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <limits>
 
 using namespace std;
-
+//struct mahasiswa
 struct mahasiswa{
     int pk;
-    int nim;
     string nama;
     string jurusan;
+    string nim;
 };
+
 //proto-type
-int getoption();
-void writedata(mahasiswa &mhs, fstream &data);
-mahasiswa bacadata(mahasiswa &baca, fstream &data);
-void checkdatabase(fstream &data);
+int getoption();    //untuk menu
+void writedata(mahasiswa &mhs ,fstream &data); //untuk nulis data ke-ekstenal file
+void readdata(mahasiswa &mhs , fstream &data); //untuk menampilkan data
 
+//body program
 int main(){
-    mahasiswa inputmahasiswa;
-    fstream file;
-    checkdatabase(file);
-    
+
+    fstream database, outdatabase;  //database ios::out | outdatabase ios::in
+    mahasiswa mahasiswapolmed, outputmhs;   //mahasiswapolmed = input | outputmhs = output
+
+    //menu
     int choice = getoption();
-    enum {create = 1, read, selesai};
+    enum {create = 1, read, size, finish};
     char next;
-
-    while (choice != selesai){
-        switch (choice){
+    
+    while(choice != finish){
+        switch (choice)        {
         case create:
-            system ("cls");
-            cout << "> Menambahkan Data Mahasiswa" << endl;
-            cout << endl;
-            writedata(inputmahasiswa, file);
+            system("cls");
+            cout << "> Menambahkan Data Mahasiswa" << endl << endl;
+            writedata(mahasiswapolmed, database);
             break;
-
+        
         case read:
-            system ("cls");
-            cout << "> Menampilkan Data Mahasiswa" << endl;
-            cout << endl;
-            bacadata(inputmahasiswa, file);
+            system("cls");
+            cout << "> Menampilkan Data Mahasiswa" << endl << endl;
+            readdata(outputmhs, outdatabase);
+            break;
+        
+        case size:
+            system("cls");
+            cout << "> Menampilkan Ukuran Data Mahasiswa" << endl << endl;
+            cout << "Ukuran Sturct : " << sizeof(mahasiswa) << endl;
+            cout << "Ukuran Variabel Struct : " << sizeof(mahasiswapolmed) << endl;
             break;
 
         default:
+            cout << "Pilihan Tidak Ditemukan !" << endl;
             break;
         }
         label_continue:
         cout << endl;
-        cout << "Lanjutkan (y/n) ? : ";
+        cout << "Lanjutkan (y/n) ? = ";
         cin >> next;
         if ((next == 'y') | (next == 'Y')){
             choice = getoption();
-        }else if((next == 'n') | (next == 'N')){
+        } else if ((next == 'n') | (next == 'N')){
             break;
-        }else {
+        } else{
             goto label_continue;
         }
     }
 
-    
-    file.close();
     cin.get();
     return 0;
 }
-//fungsi return untuk getoption
+//fungsi return untuk get option
 int getoption(){
     int input;
-    //system("cls");
-    cout << "| Program Create And Read |" << endl << endl;
+    system("cls");
+    cout << "| Program Create And Read |\n" << endl;
     cout << "[1] Tambah Data" << endl;
     cout << "[2] Baca Data" << endl;
-    cout << "[3] Selesai" << endl << endl;
-    cout << "Pilih (1-3) ? > ";
+    cout << "[3] Ukuran Data" << endl;
+    cout << "[4] Selesai" << endl << endl;
+    cout << "Pilih [1-4] ? : ";
     cin >> input;
     return input;
 }
-//fungsi void untuk nulis data
-void writedata(mahasiswa &mhs, fstream &data){
-    cout << "Nama   : "; cin >> mhs.nama;
+//fungsi void untuk write data
+void writedata(mahasiswa &mhs ,fstream &data){
+    cout << "ID. "; cin >> mhs.pk;
+    cout << "Nama   : "; getline(cin,mhs.nama);
+    cin.ignore(numeric_limits<streamsize>::max(),'\n');
     cout << "Jurusan: "; cin >> mhs.jurusan;
     cout << "Nim    : "; cin >> mhs.nim;
-    data.write(reinterpret_cast<char*> (&mhs), sizeof(mahasiswa));
+    data.open("mahasiswa.txt", ios::out | ios::app);
+    data << mhs.pk << "\t";
+    data << mhs.nama << "\t";
+    data << mhs.jurusan << "\t";
+    data << mhs.nim << endl;
+    data.close();
 }
-//fungsi return untuk baca data
-mahasiswa bacadata(mahasiswa &baca, fstream &data){
-    data.read(reinterpret_cast<char*>(&baca), sizeof(mahasiswa));
-    cout << "Nama\t" << "Prodi\t" << "NIM" << endl;
-    cout << baca.nama << "\t" << baca.jurusan << "\t" << baca.nim << endl;
-    return baca;
-}
-//fungsi void untuk checkdatabase
-void checkdatabase(fstream &data){
-	data.open("mahasiswa.bin", ios::out | ios:: in | ios::binary);
-	
-	if (data.is_open()){
-		cout << "Data ditemukan \n";
-	}else {
-		cout << "Data tidak ada dan buat baru \n";
-		data.close();
-		data.open("mahasiswa.bin", ios::trunc | ios::out | ios::in | ios::binary);
-	}
+//fungsi void untuk baca data
+void readdata(mahasiswa &mhs , fstream &data){
+    data.open("mahasiswa.txt", ios::in | ios::binary);
+    data >> mhs.pk;
+    data >> mhs.nama;
+    data >> mhs.jurusan;
+    data >> mhs.nim;
+    //tampilan diconsole
+    cout << "ID \t" << "NAMA \t\t" << "PRODI \t" << "NIM" << endl;
+    while(!data.eof()){
+        cout << mhs.pk << "\t";
+        cout << mhs.nama << "\t\t";
+        cout << mhs.jurusan << "\t";
+        cout << mhs.nim << endl;
+        
+        data >> mhs.pk;
+        data >> mhs.nama;
+        data >> mhs.jurusan;
+        data >> mhs.nim;
+    }
+    data.close();
 }
